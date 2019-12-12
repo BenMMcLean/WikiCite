f4ff603e92c2dc416d0740849144c23c43a20b95
Ben McLean <ben@origma.com.au>
2019-12-12 19:37:45 +1100

    Implemented citation hover container

--- wikicite.js
+++ wikicite.js
@@ -7,30 +7,19 @@ function Citation(id, rawCitation) {
 }
 
 function CitationConfiguration(
-	citations = [], 
-	completeCitationContainer = new CompleteCitationContainer(),
-	linkToComplete = true,
-	enableHoverContainer = true) {
+	enableCompleteCitationContainer = true,
+	completeCitationContainer = "#complete-citation-container",
+	completeCitationContainerColumns = 3,
+	enableHoverContainer = true,
+	hoverArrowOffset = 30,
+	hoverContainerFadeLength = 50) {
 		
-	this.citations = citations;
 	this.completeCitationContainer = completeCitationContainer;
-	this.linkToComplete = linkToComplete;
+	this.enableCompleteCitationContainer = enableCompleteCitationContainer;
+	this.completeCitationContainerColumns = completeCitationContainerColumns;
 	this.enableHoverContainer = enableHoverContainer;
-	
-	this.findCitation = function(id) {
-		for (var i = 0; i < citations.length; i++) {
-			if (citations[i].id == id) return citations[i];
-		}
-	}
-}
+	this.hoverArrowOffset = hoverArrowOffset;
 
-function CompleteCitationContainer(
-	root = ".complete-citation-container",
-	columns = 3) {
-	
-	this.root = root;
-	this.columns = columns;
-	
 }
 
 (function($) {
@@ -38,47 +27,106 @@ function CompleteCitationContainer(
 	var INLINE_CITATION_NAME = "data-inline-citation";
 	var LINKED_CITATION_NAME = "data-linked-citation";
 	
-	$.fn.cite = function(configuration = new CitationConfiguration()) {
-		var citations = findCitations(this);
+	var HOVER_CONTAINER_NAME = "citation-hover";
+	
+	$.fn.cite = function(citations = [], configuration = new CitationConfiguration()) {
+		new _CiteExecute(citations, configuration).execute(this);
 	}
 	
-	function findCitations(root, configuration) {
-		var usedCitations = [];
-		var usedCitationMap = {};
-		var citationCount = 0;
+	function _CiteExecute(citations, configuration) {
+		this.citations = citations;
+		this.configuration = configuration;
+		this.usedCitations = []
 		
-		root.each(function () {
-			$(this).find("["+INLINE_CITATION_NAME+"]").each(function() {
-				var citation = new Citation(null, $(this).attr(INLINE_CITATION_NAME));
-				
-				console.log(citation);
-				
-				citationCount++;
-				citation.ref = citationCount;
-				
-				usedCitations.push(citation);
-			});
+		this.execute = function(el) {
+			createHover();
+			this.processCitations(el);
+		}
+		
+		this.processCitations = function (el) {
+			var usedCitations = [];
+			var usedCitationMap = {};
+			var citationCount = 0;
 			
-			$(this).find("["+LINKED_CITATION_NAME+"]").each(function() {
-				var citation = configuration.findCitation($(this).attr(LINKED_CITATION_NAME));
-				
-				if (citation == null) return;
-				
-				citationCount++;
-				citation.ref = citationCount;
-				
-				if (!citation.id in usedCitationMap) {
+			var _this = this;
+			
+			el.each(function () {
+				$(this).find("["+INLINE_CITATION_NAME+"]").each(function() {
+					var citation = new Citation(null, $(this).attr(INLINE_CITATION_NAME));
+					
+					console.log(citation);
+					
+					citationCount++;
+					citation.ref = citationCount;
+					
 					usedCitations.push(citation);
-					usedCitationMap[citation.id] = 0;
-				}
+					
+					_this.setupCitation(this, citation);
+				});
+				
+				$(this).find("["+LINKED_CITATION_NAME+"]").each(function() {
+					var citation = _this.findCitation($(this).attr(LINKED_CITATION_NAME));
+					
+					if (citation == null) return;
+					
+					if (citation.ref == null) {
+						citationCount++;
+						citation.ref = citationCount;
+					}
+					
+					if (!(citation.id in usedCitationMap)) {
+						usedCitations.push(citation);
+						usedCitationMap[citation.id] = 0;
+					}
+					
+					_this.setupCitation(this, citation);
+				});
+				
+				this.usedCitations = usedCitations;
 			});
+		}
+		
+		this.setupCitation = function(el, citation) {
+			var _this = this;
+			var hoverable;
+			if (configuration.enableCompleteCitationContainer) {
+				hoverable = $("<a href='#ref"+ citation.ref +"' class='citation-mark'>[" + citation.ref + "]</a>").appendTo(el); 
+			} else {
+				hoverable = el;
+			}
 			
-			return usedCitations;
-		});
+			if (configuration.enableHoverContainer) {
+				$(hoverable).hover(function() {
+					var hoverableOffset = $(this).offset();
+					var hoverableHeight = $(this).height();
+					var hoverableWidth = $(this).width();
+					var me = $("#" + HOVER_CONTAINER_NAME);
+					
+					me.html(citation.citationText());
+					me.css({
+						left: hoverableOffset.left - (hoverableWidth / 2) - configuration.hoverArrowOffset,
+						top: hoverableOffset.top - me.height() - configuration.hoverArrowOffset
+					});
+					me.stop().fadeIn(_this.configuration.hoverContainerFadeLength);
+				}, function() {
+					$("#" + HOVER_CONTAINER_NAME).stop().fadeOut(_this.configuration.hoverContainerFadeLength);
+				});
+			}
+		}
+		
+		this.findCitation = function(id) {
+			for (var i = 0; i < citations.length; i++) {
+				if (citations[i].id == id) return citations[i];
+			}
+		}
 	}
 	
-	function createHoverContainer() {
+	function createHover() {
+		if ($("#" + HOVER_CONTAINER_NAME).length) {
+			return;
+		}
 		
+		$("body").append("<div id='citation-hover'></div>");
 	}
 	
 }( jQuery ));
